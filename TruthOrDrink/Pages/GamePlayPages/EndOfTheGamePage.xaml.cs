e21c4b8acbf0c2d55d9fc85236a5021878370fd2
FloydGitHub <2204860kersemakers@zuyd.nl>
2024-11-26 18:01:59 +0100

    Comments voor dingen die nog niet werken

--- TruthOrDrink/Pages/GamePlayPages/EndOfTheGamePage.xaml.cs
+++ TruthOrDrink/Pages/GamePlayPages/EndOfTheGamePage.xaml.cs
@@ -2,14 +2,26 @@ namespace TruthOrDrink.Pages.GamePlayPages;
 
 public partial class EndOfTheGamePage : ContentPage
 {
-	public EndOfTheGamePage()
-	{
-		InitializeComponent();
-	}
-	private void BackToMainMenuButton_Clicked(object sender, EventArgs e)
+    public EndOfTheGamePage()
+    {
+        InitializeComponent();
+    }
+    private async void BackToMainMenuButton_Clicked(object sender, EventArgs e)
     {
         //Gaat nu naar inlogscherm. moet naar hoofdmenu
-        Navigation.PopToRootAsync();
-        
+        await Navigation.PopToRootAsync();
+        /*
+        var homePage = Navigation.NavigationStack.First(p => p.GetType() == typeof(HomePage));
+        if (homePage != null)
+        {
+            await Navigation.PushAsync(homePage);
+            return;
+        }
+        else
+        {
+            await Navigation.PopToRootAsync();
+            return;
+        }
+        */
     }
 }

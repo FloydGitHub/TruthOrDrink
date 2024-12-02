49e270740b2f580c743fb8eece6619e5e98a3043
FloydGitHub <2204860kersemakers@zuyd.nl>
2024-12-02 18:11:26 +0100

    phone layout

--- TruthOrDrink/Pages/QuestionCrudPages/QuestionIndexPage.xaml.cs
+++ TruthOrDrink/Pages/QuestionCrudPages/QuestionIndexPage.xaml.cs
@@ -1,3 +1,6 @@
+using TruthOrDrink.Models;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace TruthOrDrink;
 
 public partial class QuestionIndexPage : ContentPage
@@ -5,7 +8,36 @@ public partial class QuestionIndexPage : ContentPage
 	public QuestionIndexPage()
 	{
 		InitializeComponent();
-	}
+        List<Question> questions = new List<Question>();
+        Category category = new Category()
+        {
+            Name = "Persoonlijk",
+            Description = "Vragen die je persoonlijk raken",
+        };
+
+        Question question = new Question()
+        {
+            Id = 1,
+            Text = "Hoe heet je?",
+            Category = category,
+            Level = 2,
+            CustomQuestion = true,
+            PhotoQuestion = false,
+        };
+        Question question2 = new Question()
+        {
+            Id = 2,
+            Text = "Wat is je favoriete kleur?",
+            Category = category,
+            Level = 1,
+            CustomQuestion = true,
+            PhotoQuestion = false,
+        };
+
+        questions.Add(question);
+        questions.Add(question2);
+        QuestionsCollectionView.ItemsSource = questions;
+    }
     private void BackButton_Clicked (object sender, EventArgs e)
     {
         Navigation.PopAsync();
